5f06689c0ebfdc0270a47fcaf3c59816321fa53f
Chu Thanh Tinh <cttinh0812@gmail.com>
2026-05-18 14:02:33 +0700

    move folder app , components and lib into src folder

--- apartment-management/src/app/layout.tsx
+++ apartment-management/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
+import { cn } from "@/src/lib/utils";
 
-const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
+const jetbrainsMono = JetBrains_Mono({ subsets: ['latin'], variable: '--font-mono' });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
