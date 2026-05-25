0295cae3f8ca1e3aedf18ec77384b1a0d8e2f983
Chu Thanh Tinh <cttinh0812@gmail.com>
2026-05-25 17:41:55 +0700

    fix vercel nest deploy

---
+++ apps/api/index.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { AppModule } from './src/app.module';
+
+let app: any;
+
+async function bootstrap() {
+    if (!app) {
+        app = await NestFactory.create(
+            AppModule,
+            new FastifyAdapter(),
+        );
+
+        await app.init();
+    }
+
+    return app;
+}
+
+export default async function handler(req: any, res: any) {
+    const app = await bootstrap();
+
+    app
+        .getHttpAdapter()
+        .getInstance()
+        .server.emit('request', req, res);
+}
