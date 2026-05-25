5cbca622b77814d642b302b44dd8cf8c1cb735b6
Chu Thanh Tinh <cttinh0812@gmail.com>
2026-05-25 17:58:13 +0700

    update file index.ts

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,3 +1,27 @@
-import handler from '../index';
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { AppModule } from '../src/app.module';
 
-export default handler;
+let app: any;
+
+async function bootstrap() {
+    if (!app) {
+        app = await NestFactory.create(
+            AppModule,
+            new FastifyAdapter(),
+        );
+
+        await app.init();
+    }
+
+    return app;
+}
+
+export default async function handler(req: any, res: any) {
+    const app = await bootstrap();
+
+    app
+        .getHttpAdapter()
+        .getInstance()
+        .server.emit('request', req, res);
+}
