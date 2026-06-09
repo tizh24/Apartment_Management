57ca291a6441be0fe5b10457a142b26e7d545371
phamhung116 <phamhungdn116@gmail.com>
2026-06-09 19:47:49 +0700

    refactor: apply design pattern to contracts module

--- apps/api/src/modules/contracts/contracts.module.ts
+++ apps/api/src/modules/contracts/contracts.module.ts
@@ -1,12 +1,18 @@
 import { Module } from "@nestjs/common";
 
 import { AuthModule } from "../auth/auth.module";
+import { ContractsCommandService } from "./commands/contracts-command.service";
 import { ContractsController } from "./contracts.controller";
-import { ContractsService } from "./contracts.service";
+import { ContractsQueryService } from "./queries/contracts-query.service";
+import { ContractsRepository } from "./repositories/contracts.repository";
 
 @Module({
   imports: [AuthModule],
   controllers: [ContractsController],
-  providers: [ContractsService],
+  providers: [
+    ContractsRepository,
+    ContractsCommandService,
+    ContractsQueryService,
+  ],
 })
 export class ContractsModule {}
